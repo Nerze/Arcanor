f8b85c8531d61ea66567ae5bd991496265373910
CommandantB <xngt974@gmail.com>
2018-05-15 16:27:27 +0200

    Delete StartMenu.java

--- src/StartMenu.java
+++
@@ -1,9 +0,0 @@
-package arcanor;
-public class StartMenu extends Menu{
-	private int playerNb;
-	private ParamMenu startParams;
-	public StartMenu(String title, int nbJoueurs){}
-	public int getPlayerNb(){}
-	public void setPlayerNb(int playerNb){}
-	public void setNomJoueurs(String[] nomJoueurs){}
-	public ParamMenu getStartParams(){}
