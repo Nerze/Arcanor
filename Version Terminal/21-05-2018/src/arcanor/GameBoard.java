fb78655ef2ef18d79f49e3c38bf7a4939185df4c
Nerze <38243289+Nerze@users.noreply.github.com>
2018-05-22 11:15:23 +0200

    Add files via upload

--- Version Terminal/21-05-2018/src/arcanor/GameBoard.java
+++ Version Terminal/21-05-2018/src/arcanor/GameBoard.java
@@ -8,11 +8,37 @@ public class GameBoard{
    private ArrayList<Player> playerList;
    private Piece[][] board;
 
-   public GameBoard(ParamMenu params, ArrayList<Player> list){}
+   public GameBoard(ParamMenu params, ArrayList<Player> list){
+	   if(params==null){
+		   System.out.println("GameBoard() ERROR : params is null");
+		   params= new ParamMenu();
+	   }
+	   if(list==null){
+		   System.out.println("GameBoard() ERROR : list is null");
+		   list=new ArrayList<Player>();
+		   list.add(new Player(true,"Player"));
+		   list.add(new Player(false,"AI"));
+	   }
+   }
    public void save(){}
-   public int getTurn(){}
-   public void setTurn(int turn){}
+   public int getTurn(){return this.turn;}
+   public void setTurn(int turn){
+		if(turn>0){
+			this.turn=turn;
+		}
+		else{
+			System.out.println("setTurn() ERROR : turn invalid");
+		}
+   }
    public void play(){}
-   public void display(){}
+   public void display(){
+	   System.out.println("Turn "+this.turn);
+	   for(int i=0;i<this.board[0].length;i++){
+		   for(int j=0;j<this.board.length;j++){
+			   System.out.print(this.board[j][i].toString());
+		   }
+		   System.out.print("\n");
+	   }
+   }
 
 }
--- Version Terminal/21-05-2018/src/arcanor/Player.java
+++ Version Terminal/21-05-2018/src/arcanor/Player.java
@@ -5,7 +5,16 @@ public class Player{
    private boolean human;
    private String name;
 
-   public Player(boolean type, String name){}
-   public int[][] playTurn(){}
+   public Player(boolean type, String name){
+	   this.human=type;
+	   if(name==null){
+		   name="";
+		   Sytem.out.println("Player() ERROR : name is null");
+	   }
+	   this.name=name;
+   }
+   
+   
+   abstract public int[] playTurn();
       
 }
