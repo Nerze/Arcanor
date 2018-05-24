929957f654cf23d1ebdfe2a6711aad20e54f96cb
Nerze <38243289+Nerze@users.noreply.github.com>
2018-05-24 09:15:38 +0200

    Update GameMenu.java

--- Version Terminal/21-05-2018-JAVADOC/src/arcanor/GameMenu.java
+++ Version Terminal/21-05-2018-JAVADOC/src/arcanor/GameMenu.java
@@ -1,11 +1,28 @@
 package arcanor;
+import java.util.ArrayList;
 import System.out;
 
 /**
  * Cette classe est le menu permettant de rediriger vers les pages de lancement de parties
  */
 public class GameMenu extends Menu{
-
+	private int turn;
+	private ParamMenu gameParams;
+	private ArrayList<Player> playerList;
+	private Piece[][] board;
+	
+	public GameBoard(ParamMenu params, ArrayList<Player> list){
+	   if(params==null){
+		   println("GameBoard() ERROR : params is null");
+		   params= new ParamMenu();
+	   }
+	   if(list==null){
+		   println("GameBoard() ERROR : list is null");
+		   list=new ArrayList<Player>();
+		   list.add(new Player(true,"Player"));
+		   list.add(new Player(false,"AI"));
+	   }
+   	}
 	/**
 	 * Fonction menant vers StartMenu
 	 */
