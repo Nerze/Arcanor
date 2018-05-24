2b8052829cf80ec918f87e92c30c5c75b92f8515
nirvigo <39306061+nirvigo@users.noreply.github.com>
2018-05-24 08:55:28 +0200

    Add files via upload

--- Version Terminal/21-05-2018-JAVADOC/src/arcanor/ParamMenu.java
+++ Version Terminal/21-05-2018-JAVADOC/src/arcanor/ParamMenu.java
@@ -5,38 +5,75 @@ package arcanor;
  */
 public class ParamMenu extends Menu{
 
-   /**
-    * tableau de paramètres codés
-    */
-   private String[] tabParams;
+	/**
+     * tableau de paramètres codés
+     * 1er emplacement difficulté
+     * 2ème emplacement nb joueur
+     */
+    private String[] tabParams;
 
-   /**
-    * Le contructeur
-    */
-   public ParamMenu(){}
+    /**
+     * Le contructeur
+     */
+    public ParamMenu(){
+	
+		this.tabParams = ["0", "0"]
+	   
+	}
 
-   /**
-    * Getter de tabParams
-    * @return le tableau de paramètres
-    */
-   public String[] getTabParams(){}
+    /**
+     * Getter de tabParams
+     * @return le tableau de paramètres
+     */
+    public String[] getTabParams(){
+		
+		return this.tabParams;
+		
+	}
 
-   /**
-    * Setter de tabParams
-    * @param tabParams le tableau de paramètres
-    */
-   public void setTabParams(String[] tabParams){}
+    /**
+     * Setter de tabParams
+     * @param tabParams le tableau de paramètres
+     */
+    public void setTabParams(String[] tabParams){
+		
+		this.tabParams = tabParams;
+		
+	}
 
-   // public void setDifficulte(int niveau){}
-   //
-   /**
-   * Fonction d'affichage
-   */
-   public void display(){}
+    public void setDifficulte(int niveau){
+		
+		this.tabParams[0] = "1";
+		
+	}
+    
+    /**
+     * Fonction d'affichage
+     */
+	public void display(){
+	   
+		System.out.println("1. changer de difficulté (non accessible en version beta)");
+		System.out.println("2. changer le nombre de joueur (non accessible en version beta)");
+		System.out.println("3. Retour au menu de lancement");
+		
+		int i = System.in;
+		while (i != 3) {}
+		
+		if (i = 3) {
+			
+			back();
+			
+		}
+	   
+	}
 
-   /**
-   * Fonction de retour arrière du menu
-   */
-   public void back(){}
+    /**
+     * Fonction de retour arrière du menu
+     */
+    public void back(){
+		
+		Runtime.getRuntime().exec("clear");
+		
+	}
       
 }
