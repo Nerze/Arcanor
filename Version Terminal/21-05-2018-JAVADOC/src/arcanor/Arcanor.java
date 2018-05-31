ebc921a4bf89524618ce03182126b7f9f05b7f9d
CommandantB <xngt974@gmail.com>
2018-05-31 11:47:42 +0200

    Update Arcanor.java

--- Version Terminal/21-05-2018-JAVADOC/src/arcanor/Arcanor.java
+++ Version Terminal/21-05-2018-JAVADOC/src/arcanor/Arcanor.java
@@ -2,21 +2,20 @@ package arcanor;
 import System.out;
 
 /**
- * Cette classe est la classe qui lance le jeu
+ * This class is the main entry, the game launcher
  */
 public class Arcanor{
 
    /**
-    * Vrai si jeu fini
+    * True is the game end
     */
    private boolean end;
 
    /**
-    * Routine principale de lancement
-    * @param arg arguments possibles
+    * The main entry of the program that launch the game
+    * @param arg optionnal arguments
     */
    public static void main(String[] arg) {
-
+      // TO DO
    }
-
 }
