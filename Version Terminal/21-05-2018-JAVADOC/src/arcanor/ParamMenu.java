457aad7a273b5aa387f383b4831d1a55cdc1c9dc
CommandantB <xngt974@gmail.com>
2018-06-18 15:17:51 +0200

    Update ParamMenu.java

--- Version Terminal/21-05-2018-JAVADOC/src/arcanor/ParamMenu.java
+++ Version Terminal/21-05-2018-JAVADOC/src/arcanor/ParamMenu.java
@@ -9,8 +9,8 @@ public class ParamMenu extends Menu{
 
 	/**
 	 * Coded settings into a table
-	 * tab[0] - Vertical dimension of GameBoard
-	 * tab[1] - Horizontal dimension of GameBoard
+	 * tab[0] - (WIDTH) Vertical dimension of GameBoard
+	 * tab[1] - (HEIGHT) Horizontal dimension of GameBoard
 	 * tab[2] - Number of Human Player
 	 * tab[3] - Player 1 name
 	 * tab[4] - Player 2 name
@@ -21,7 +21,8 @@ public class ParamMenu extends Menu{
 	 * The constructor
 	 */
 	public ParamMenu(){
-		this.tabParams = ["0", "0"]
+		this.tabParams = ["7","8","1","Red","Blue"],
+		this.display();
 	}
 
 	/**
@@ -36,36 +37,87 @@ public class ParamMenu extends Menu{
      * Setter de tabParams
      * @param tabParams le tableau de paramètres
      */
-    public void setTabParams(String[] tabParams){
-		this.tabParams = tabParams;
+    public void setTabParams(String[] newTab){
+	    	if (newTab.legnth() == tabParams.length()){
+			this.tabParams = tabParams;
+		}else{
+			System.err.println("ERROR - setTabParams - newTab.legnth() != tabParams.length()");
+		}
 	}
     
     /**
      * Printing method
      */
 	public void display(){
-	   
-		System.out.println("1. changer de difficulté (non accessible en version beta)");
-		System.out.println("2. changer le nombre de joueur (non accessible en version beta)");
-		System.out.println("3. Retour au menu de lancement");
-		
 		Scanner sc = new Scanner(System.in);
-		int i = sc.nextInt();
-		while (i != 3) {}
+
+	   	Runtime.getRuntime().exec("clear");
+		System.out.println("==== Arcanor V0.1 ====");
+		System.out.println("= 1. Change Board dimensions");
+		System.out.println("= 2. Change Player's number");
+		System.out.println("= 3. Change Player 1 name");
+		System.out.println("= 4. Change Player 2 name");
+		System.out.println("= 0. Back");
 		
-		if (i = 3) {
+		int i = sc.nextInt();
+		while (i != 0) {
+			if (i == 1) {
+			   	Runtime.getRuntime().exec("clear");
+				System.out.println("==== Arcanor V0.1 ====");
+				System.out.println("= Set Width : (8 by default)");
+				
+				int j = sc.nextInt();
+				while (j <= 0) {
+					System.err.println("Humm. It seems to be a number under 0, try with a positive one.");
+					j = sc.nextInt();
+				}
+				this.tabParams[0] = Integer.toString(j);
+
+				System.out.println("= Set Height : (7 by default)");
+				j = sc.nextInt();
+				while (j <= 0) {
+					System.err.println("Humm. It seems to be a number under 0, try with a positive one.");
+					j = sc.nextInt();
+				}
+				this.tabParams[1] = Integer.toString(j);
 			
-			back();
+			}else if (i == 2) {
+				Runtime.getRuntime().exec("clear");
+				System.out.println("==== Arcanor V0.1 ====");
+				System.out.println("= Set number of players :");
 			
+				int j = sc.nextInt();
+				while ((j < 0) && (j > 2)) {
+					System.err.println("Humm. You can't play without at least 1 player and at last 2 players.");
+					j = sc.nextInt();
+				}
+				this.tabParams[2] = Integer.toString(j);
+			
+			}else if (i == 3) {
+				Runtime.getRuntime().exec("clear");
+				System.out.println("==== Arcanor V0.1 ====");
+				System.out.println("= Set Player 1 name :");
+				this.tabParams[3] = sc.nextLine();
+			
+			}else if (i == 4) {
+				Runtime.getRuntime().exec("clear");
+				System.out.println("==== Arcanor V0.1 ====");
+				System.out.println("= Set Player 2 name :");
+				this.tabParams[4] = sc.nextLine();
+			
+			}else if (i == 0) {
+				this.back();
+			
+			}else{
+				System.err.println("Humm. It seems like you wrote a wrong input. Try again please");
+			}
 		}
-	   
 	}
 
 	/**
 	 * Method to get back to the previous menu
 	 */
 	public void back(){
-		Runtime.getRuntime().exec("clear");
+		this.return;
 	}
-      
 }
