53b1977003e6816c3054e0ea07248fb6613a9443
Nerze <38243289+Nerze@users.noreply.github.com>
2018-06-18 15:24:48 +0200

    Add files via upload

--- Version Terminal/21-05-2018-JAVADOC/src/arcanor/Board.java
+++ Version Terminal/21-05-2018-JAVADOC/src/arcanor/Board.java
@@ -43,23 +43,25 @@ public class Board{
 	}
 	
 	
-	public boolean movePiece(int xOr,int yOr, int xDest, int yDest){
-		boolean ret=true;
+	public boolean movePiece(int xOr,int yOr, int xDest, int yDest, boolean colorPlayer){
+		boolean ret=false;
 		if(xOr-xDest<=1 && xDest-xOr<=1 && yOr-yDest<=1 && yDest-yOr<=1 && yDest<this.height && yDest>=0 && yOr<this.height && yOr>=0 && xDest<this.width && xDest>=0 && xOr<this.width && xOr>=0){
 			if(this.grid[xOr][yOr]!=null){
-				if(this.grid[xDest][yDest]==null){//There is no piece at the destination
-					this.grid[xDest][yDest]=this.grid[xOr][yOr];
-					this.grid[xOr][yOr]=null;
-					ret=true;
-				}
-				else{//There is a piece at the destination
-					if(this.grid[xDest][yDest].getValue()==this.grid[xOr][yOr].getValue()-1 && this.grid[xDest][yDest].getColor()!=this.grid[xOr][yOr].getColor()){
-						//If you can eat the piece
-						Piece or = this.grid[xOr][yOr];
-						this.grid[xOr][yOr]=or.getContain();//The inside of the moving piece is the new piece on this spot
-						or.setContain(this.grid[xDest][yDest]);//put the piece in the bigger one
-						this.grid[xDest][yDest]=or;//Move it to the destination
+				if(this.grid[xOr][yOr].color==colorPlayer){//The player tries to move its own piece
+					if(this.grid[xDest][yDest]==null){//There is no piece at the destination
+						this.grid[xDest][yDest]=this.grid[xOr][yOr];
+						this.grid[xOr][yOr]=null;
 						ret=true;
+					}
+					else{//There is a piece at the destination
+						if(this.grid[xDest][yDest].getValue()==this.grid[xOr][yOr].getValue()-1 && this.grid[xDest][yDest].getColor()!=this.grid[xOr][yOr].getColor()){
+							//If you can eat the piece
+							Piece or = this.grid[xOr][yOr];
+							this.grid[xOr][yOr]=or.getContain();//The inside of the moving piece is the new piece on this spot
+							or.setContain(this.grid[xDest][yDest]);//put the piece in the bigger one
+							this.grid[xDest][yDest]=or;//Move it to the destination
+							ret=true;
+						}
 					}
 				}
 			}
