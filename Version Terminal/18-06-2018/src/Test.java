87fdd2d7bc00c5bccf710880fd075a720f098c1b
Nerze <38243289+Nerze@users.noreply.github.com>
2018-06-20 16:12:00 +0200

    Add files via upload

--- Version Terminal/18-06-2018/src/Test.java
+++ Version Terminal/18-06-2018/src/Test.java
@@ -1,10 +1,11 @@
 import arcanor.*;
+import java.io.*;
 public class Test{
 	public static void main(String[] args){
-		for(int i=0;i<11;i++){
-			Board board=new Board(i,i*3,1);
-			board.movePiece(0,0,1,1,true,true);
-			System.out.println(board.displayGrid());
-		}
+		ParamMenu param = new ParamMenu();
+		String[] params = {"7","3","2","Red","Blue","12"};
+		param.setTabParams(params);
+		Game game = new Game(param);
+		game.start();
 	}
 }
