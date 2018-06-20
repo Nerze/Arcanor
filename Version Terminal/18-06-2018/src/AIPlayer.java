de42266f3fdc9603623e3c8e665f52052baed135
CommandantB <xngt974@gmail.com>
2018-06-20 17:56:14 +0200

    Add files via upload

--- Version Terminal/18-06-2018/src/AIPlayer.java
+++ Version Terminal/18-06-2018/src/AIPlayer.java
@@ -17,38 +17,96 @@ public class AIPlayer extends Player{
 	}
 	
 	/**
+	 * 
+	 */
+	private boolean isFree(int w, int h){
+		boolean ret;
+		if ((w > 0) && (h > 0) && (w < this.board.getWidth()-2) && (h < this.board.getHeight)
+		//TO DO
+	}
+	
+	/**
 	 * The main thread to play
 	 */
-	public void playTurn(){
+	public void playTurn(int turn){
 		System.out.println(this.board.displayGrid());
 		boolean valid = false, played = false;
 		Random r = new Random();
 		int width, height, choosen;
-		Piece cho;
+
 		while(!played){
 			width = r.nextInt(this.board.getWidth());
 			height = r.nextInt(this.board.getHeight());
-			if((this.board.getPiece(width, height) != null) && (this.board.getPiece(width, height).getColor == this.color){
-				System.out.println(name + " take the piece : " + width + "," + height);
+			boolean free = this.isFree(width, height);
+			if((this.board.getPiece(width, height) != null) && (this.board.getPiece(width, height).getColor() == this.color) && (free)){
+				System.out.println(name + " took the piece : Row " + (width+1) + ", Line " + (height+1));
 				while(!valid){
 					choosen = r.nextInt(8);
 					switch(choosen){
 						case 1 :
-							valid = movePiece(width, height, width, height+1, this.color, r.nextBoolean());
+							if (this.board.movePiece(width, height, width, height+1, this.color, r.nextBoolean())){
+								valid = true;
+								played = true;
+								System.out.println(valid+" "+played);
+								System.out.println("And play it in : Row " + (width+1) + ", Line " + (height+2));
+							}
+							break;
 						case 2 :
-							valid = movePiece(width, height, width+1, height+1, this.color, r.nextBoolean());
+							if (this.board.movePiece(width, height, width+1, height+1, this.color, r.nextBoolean())){
+								valid = true;
+								played = true;
+								System.out.println(valid+" "+played);
+								System.out.println("And play it in : Row " + (width+2) + ", Line " + (height+2));
+							}
+							break;
 						case 3 :
-							valid = movePiece(width, height, width+1, height, this.color, r.nextBoolean());
+							if (this.board.movePiece(width, height, width+1, height, this.color, r.nextBoolean())){
+								valid = true;
+								played = true;
+								System.out.println(valid+" "+played);
+								System.out.println("And play it in : Row " + (width+2) + ", Line " + (height+1));
+							}
+							break;
 						case 4 :
-							valid = movePiece(width, height, width+1, height-1, this.color, r.nextBoolean());
+							if (this.board.movePiece(width, height, width+1, height-1, this.color, r.nextBoolean())){
+								valid = true;
+								played = true;
+								System.out.println(valid+" "+played);
+								System.out.println("And play it in : Row " + (width+2) + ", Line " + (height));
+							}
+							break;
 						case 5 :
-							valid = movePiece(width, height, width, height-1, this.color, r.nextBoolean());
+							if (this.board.movePiece(width, height, width, height-1, this.color, r.nextBoolean())){
+								valid = true;
+								played = true;
+								System.out.println(valid+" "+played);
+								System.out.println("And play it in : Row " + (width+1) + ", Line " + (height));
+							}
+							break;
 						case 6 :
-							valid = movePiece(width, height, width-1, height-1, this.color, r.nextBoolean());
+							if (this.board.movePiece(width, height, width-1, height-1, this.color, r.nextBoolean())){
+								valid = true;
+								played = true;
+								System.out.println(valid+" "+played);
+								System.out.println("And play it in : Row " + (width) + ", Line " + (height));
+							}
+							break;
 						case 7 :
-							valid = movePiece(width, height, width-1, height, this.color, r.nextBoolean());
+							if (this.board.movePiece(width, height, width-1, height, this.color, r.nextBoolean())){
+								valid = true;
+								played = true;
+								System.out.println(valid+" "+played);
+								System.out.println("And play it in : Row " + (width) + ", Line " + (height+1));
+							}
+							break;
 						case 0 :
-							valid = movePiece(width, height, width-1, height+1, this.color, r.nextBoolean());
+							if (this.board.movePiece(width, height, width-1, height+1, this.color, r.nextBoolean())){
+								valid = true;
+								played = true;
+								System.out.println(valid+" "+played);
+								System.out.println("And play it in : Row " + (width) + ", Line " + (height+2));
+							}
+							break;
 					}
 				}
 			}
